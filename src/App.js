8952c74ed9d23e314140ed0f70c52203b8c47b20
Mohammed Alam <mohammedalam52@gmail.com>
2022-01-12 14:26:11 -0500

    added form for zipcode input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      Results: [],  
+    };
+  }
+  handleInput = (event) => {
+    this.setState({items: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+  }
+
+  componentWillMount() {
+    fetch("http://ctp-zip-api.herokuapp.com/zip/10016")
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({
+          items: response,
+        });
+      });
+  }
+
+  render() {
+    let { items } = this.state;
+
+    return (
+      <div className="App">
+      <form className="input-bar" onSubmit={this.handleSubmit}>
+            <p>Zip Code:</p>
+            <input placeholder="Input a Zip Code" onChange={this.handleInput}></input>
+          </form>
+        <ul>
+          {items.map((item) => (
+            <li kenpy={item.id}>
+              State: {item.State}
+              <br />
+              Zipcode: {item.Zipcode}
+              <br />
+              Location: ({item.Lat}, {item.Long})
+              <br />
+              Population(estimated) : {item.EstimatedPopulation}
+              <br />
+              Total Wages: {item.TotalWages}
+            </li>
+          ))}
+        </ul>
+        <ZipResults results={this.state.results} />
+      </div>
+    );
+  }
 }
 
 export default App;
